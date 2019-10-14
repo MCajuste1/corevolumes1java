a460904be1a40d0b40fe65c90559fe723909bb82
Malcolm Cajuste <malcolmacajuste@gmail.com>
2019-10-14 12:31:55 -0400

    Initial commit

---
+++ src/com/somethingcore/Queue.java
@@ -0,0 +1,35 @@
+package com.somethingcore;
+
+public class Queue {
+
+/*
+    public class CircularArrayQueue<E> implements Queue<E> // not an actual library class {
+    private int head;
+    private int tail;
+     CircularArrayQueue(int capacity) { . . . }
+     public void add(E element) { . . . }
+    public E remove() { . . . }
+    public int size() { . . . }
+    private E[] elements; }
+    public class LinkedListQueue<E> implements Queue<E> // not an actual library class
+    {   private Link head;
+    private Link tail;
+     LinkedListQueue() { . . . }
+     public void add(E element) { . . . }
+      public E remove() { . . . }
+         public int size() { . . . } }
+
+
+
+     - A queue is collection for holding elements before processing
+     -  provide additional insertion, removal, and inspection operations
+     -
+
+
+ */
+
+
+
+
+
+}
---
+++ src/com/somethingcore/SortedSet.java
@@ -0,0 +1,23 @@
+package com.somethingcore;
+
+public class SortedSet {
+    /*
+    Comparator<? super E> comparator()
+    returns the comparator used for sorting the elements, or null if the elements are compared with the compare to method of the Comparable interface.
+    E first()
+    E last()
+    returns the smallest or largest element in the sorted set
+
+
+
+
+
+
+
+     */
+
+
+
+
+
+}
